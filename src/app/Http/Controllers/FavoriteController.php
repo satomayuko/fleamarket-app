0e49efcee2618691b172b7f333d8a24e5266a17f
satomayuko <puru_zas13_9@yahoo.co.jp>
2025-11-04 13:41:54 +0900

    FavoriteController.php修正

--- src/app/Http/Controllers/FavoriteController.php
+++ src/app/Http/Controllers/FavoriteController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Item;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 
 class FavoriteController extends Controller
 {
-    public function store(Item $item)
+    public function store(Item $item): RedirectResponse
     {
+        if ($item->user_id === Auth::id()) {
+            abort(403);
+        }
+
         Auth::user()
             ->favoriteItems()
             ->syncWithoutDetaching([$item->id]);
@@ -16,8 +21,12 @@ class FavoriteController extends Controller
         return back()->with('success', 'いいねしました');
     }
 
-    public function destroy(Item $item)
+    public function destroy(Item $item): RedirectResponse
     {
+        if ($item->user_id === Auth::id()) {
+            abort(403);
+        }
+
         Auth::user()
             ->favoriteItems()
             ->detach($item->id);
