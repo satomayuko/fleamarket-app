b47ef34a90ee4479bc1e458a5a279f9b60da59f3
satomayuko <puru_zas13_9@yahoo.co.jp>
2025-11-04 13:08:00 +0900

    final: 不要ファイル削除・profile.blade.php整形最終版

--- src/resources/views/profile.blade.php
+++ src/resources/views/profile.blade.php
@@ -14,17 +14,32 @@
 
         <div class="user-info">
             <div class="user-info__avatar">
-                <img src="{{ $profile->avatar_path ? asset('storage/' . $profile->avatar_path) : asset('images/avatar-placeholder.png') }}" alt="" aria-hidden="true">
+                <img 
+                    src="{{ $profile->avatar_path ? asset('storage/' . $profile->avatar_path) : asset('images/avatar-placeholder.png') }}" 
+                    alt="" 
+                    aria-hidden="true"
+                >
             </div>
+
             <div class="user-info__name">{{ $user->name }}</div>
-            <a href="{{ route('mypage.profile.edit') }}" class="user-info__edit">プロフィールを編集</a>
+
+            <a href="{{ route('mypage.profile.edit') }}" class="user-info__edit">
+                プロフィールを編集
+            </a>
         </div>
 
         <div class="profile-tabs">
-            <a href="{{ route('mypage', ['tab' => 'sell']) }}" class="tab {{ $tab === 'sell' ? 'is-active' : '' }}">
+            <a 
+                href="{{ route('mypage', ['tab' => 'sell']) }}" 
+                class="tab {{ $tab === 'sell' ? 'is-active' : '' }}"
+            >
                 出品した商品
             </a>
-            <a href="{{ route('mypage', ['tab' => 'buy']) }}" class="tab {{ $tab === 'buy' ? 'is-active' : '' }}">
+
+            <a 
+                href="{{ route('mypage', ['tab' => 'buy']) }}" 
+                class="tab {{ $tab === 'buy' ? 'is-active' : '' }}"
+            >
                 購入した商品
             </a>
         </div>
@@ -37,19 +52,25 @@
                             $src = $item->cover_image ?? '';
                             $isUrl = $src && (str_starts_with($src, 'http://') || str_starts_with($src, 'https://'));
                         @endphp
+
                         @if ($src)
-                            <img src="{{ $isUrl ? $src : asset('storage/' . $src) }}"
-                                 alt="{{ e($item->title) }}"
-                                 onerror="this.src='{{ asset('images/placeholder-290.png') }}';this.onerror=null;">
+                            <img 
+                                src="{{ $isUrl ? $src : asset('storage/' . $src) }}"
+                                alt="{{ e($item->title) }}"
+                                onerror="this.src='{{ asset('images/placeholder-290.png') }}';this.onerror=null;"
+                            >
                         @else
                             <img src="{{ asset('images/placeholder-290.png') }}" alt="no image">
                         @endif
                     </div>
+
                     <p class="product-name">{{ e($item->title) }}</p>
                 </a>
             @empty
                 <div class="empty">
-                    <p>{{ $tab === 'sell' ? 'まだ出品はありません。' : 'まだ購入履歴はありません。' }}</p>
+                    <p>
+                        {{ $tab === 'sell' ? 'まだ出品はありません。' : 'まだ購入履歴はありません。' }}
+                    </p>
                 </div>
             @endforelse
         </div>
